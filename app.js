2aab2192b997a74c1aec6f48bef934f38b978109
Merrick Chun <merrickc@Blues.local>
2016-10-05 15:13:41 -0400

    Add navigation highlighting functionality

--- app.js
+++ app.js
@@ -1,42 +1,85 @@
 (function(){
-	var app = angular.module('merrick', []);
+	// Create the module and name it app
+		// include ngRoute for page routing
+	var app = angular.module('merrick', ['ngRoute']);
+
+	//Configure routes
+    app.config(function($routeProvider) {
+        $routeProvider
+            // route for the home page
+            .when('/', {
+                templateUrl : 'templates/about.html',
+                controller  : 'aboutController',
+                controllerAs: 'aCtrl'
+            })
+
+            // route for the front-end page
+            .when('/front-end', {
+                templateUrl : 'templates/front-end.html',
+                controller  : 'frontController',
+                controllerAs: 'fCtrl'
+            })
+
+            // route for the design page
+            .when('/design', {
+                templateUrl : 'templates/design.html',
+                controller  : 'designController',
+                controllerAs: 'dCtrl'
+            })
+
+            // route for the projects page
+            .when('/projects', {
+                templateUrl : 'templates/projects.html',
+                controller  : 'projectsController',
+                controllerAs: 'pCtrl'
+            })
+
+            .otherwise({ redirectTo: '/' });
+    });
+
+	app.controller('navController', function($scope){
+		$scope.isActive = function (path) {
+			var urlPath = window.location.href;
+			var urlPage = urlPath.substr(urlPath.indexOf("#/")+1);
+		 	return path === urlPage;
+		}
+	});
+
+    app.controller('aboutController', function($scope){
+    	$scope.message = 'About page!';
+    });
+
+    app.controller('frontController', function($scope){
+    	$scope.message = 'Front-end page!';
+    });
+
+    app.controller('designController', function($scope){
+    	$scope.message = 'Design page!';
+    	this.design = designItems;
+    });
+
+    app.controller('projectsController', function($scope){
+    	$scope.message = 'Projects page!';
+    });
 
 	app.directive('navigation', function(){
 		return{
 			restrict: 'E',
-			templateUrl: 'templates/navigation.html',
-			controller: function(){
-				tab = 0;
-				this.linkslist = links;
-
-				this.selectTab = function(setTab){
-					tab = setTab;
-				};
-				this.isSelected = function(checkTab){
-					return tab === checkTab;
-				};
-			},
-			controllerAs: 'panel'
+			templateUrl: 'templates/navigation.html'
 		}
 	});
 
-	var links = [
-		{
-			id:'0',
-			name:'About'
-		},
+	var designItems = [
 		{
 			id:'1',
-			name:'Front-end'
+			name: 'Striped Media',
+			image: 'images/striped.jpg'
 		},
 		{
 			id:'2',
-			name:'Design'
+			name: 'Plicpad',
+			image: 'images/plicpad.jpg'
 		},
-		{
-			id:'3',
-			name:'Projects'
-		}
 	];
 
 })();
--- files/js.js
+++ files/js.js
@@ -2,5 +2,5 @@ $(document).ready(function(){
 	//Animate logo to fade in after 2 second delay
 	$(".app-container").animate({
 		opacity:1
-	}, 500);
+	}, 500);	
 });
